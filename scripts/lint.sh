d5ac531ddc6967d52d846968d5a7953ea40f0409
Gonzalo Gómez Gracia <gongomgra@users.noreply.github.com>
2023-05-16 10:51:53 +0200

    Configure GitHub Actions (#8)
    * Configure GitHub Actions
    
    Signed-off-by: Gonzalo Gomez Gracia <gonzalog@vmware.com>

--- scripts/lint.sh
+++ scripts/lint.sh
@@ -1,27 +1,61 @@
 #!/bin/bash
 
 set -u
-errorFiles=()
-exitCodeSum=0
+read -a files_to_check <<< "$@"
+error_files=()
+exit_code_sum=0
+FORCE=${FORCE:-0}
 
-while read -r f; do
-    # Check both 'name' key is present and it is a valid JSON file
-    echo "Checking '$(basename "$f")'"
-    output="$(jq --exit-status ".name" "$f")"
-    exitCode=$?
-    if [ $exitCode -ne 0 ]; then
-        errorFiles+=("$(basename "$f")")
-        # jq doesn't return any error message if 'name' is not found, just 'null'.
-        # Let the user know about this specific issue
-        if [ "$output" = "null" ] ; then
-            echo "error: 'name' key not found"
+if [[ "$FORCE" -eq 0 ]] && [[ "${#files_to_check[@]}" -eq 0 ]]; then
+    echo "No files provided, set FORCE=1 to lint all files. Skipping linter!"
+    exit 0
+elif [[ "$FORCE" -eq 1 ]] && [[ "${#files_to_check[@]}" -eq 0 ]]; then
+    echo "No files provided but FORCE=1. Will lint all files"
+    # Add all files to the array
+    while read -r f; do
+        files_to_check+=("$f")
+    done < <(find . -type f \( -name "*.json" -o -name "*.md" \))
+fi
+
+if [[ "${#files_to_check[@]}" -ne 0 ]]; then
+    for f in "${files_to_check[@]}"; do
+        # Run a different linter depending on file extension
+        base_file="$(basename -- "$f")"
+        echo "Checking '$base_file'"
+        file_extension="${base_file##*.}"
+        if [[ "$file_extension" = "json" ]]; then
+            jq_request="."
+            if [[ "$f" =~ .*\/?config\/.* ]]; then
+                # Check both 'name' key is present and it is a valid JSON file
+                jq_request=".name"
+            fi
+            output="$(jq --exit-status "${jq_request}" "$f")"
+            exit_code=$?
+            if [[ $exit_code -ne 0 ]]; then
+                # jq doesn't return any error message if 'name' is not found, just 'null'.
+                # Let the user know about this specific issue
+                if [[ "$output" = "null" ]] && [[ "$jq_request" = ".name" ]]; then
+                    echo "error: 'name' key not found"
+                fi
+            fi
+        elif [[ "$file_extension" = "md" ]]; then
+            # MD013: allow long lines in file
+            markdownlint --disable=MD013 "$f"
+            exit_code=$?
+        else
+            echo "Skipping '$base_file'"
+            continue
         fi
-    fi
-    exitCodeSum=$((exitCodeSum + exitCode))
-done < <(find ./config -type f -name "*.json")
+        # Check error code to report errors
+        if [[ $exit_code -ne 0 ]]; then
+            error_files+=("$(basename "$f")")
+        fi
+        exit_code_sum=$((exit_code_sum + exit_code))
+    done
+fi
 
-if [ $exitCodeSum -ne 0 ]; then
-    echo "Errors found in the next files: '${errorFiles[@]}'. Please review them"
+if [[ $exit_code_sum -ne 0 ]]; then
+    echo "Errors found in the next files: '${error_files[@]}'. Please review them"
     exit 1
 fi
 exit 0
